68542d8118df30d116846111688614bae0cc3b3e
ShengqiWang <haoruw@outlook.com>
2019-11-03 18:56:11 +0800

    44

--- cpp_homework_gui_version/calculator_gui.cpp
+++ cpp_homework_gui_version/calculator_gui.cpp
@@ -1,5 +1,7 @@
 #include "calculator_gui.h"
 #include <string>
+#include <QApplication>
+#include <algorithm>
 //using namespace std;
 
 calculator_gui::calculator_gui(QWidget* parent) :QWidget(parent)
@@ -48,13 +50,33 @@ void calculator_gui::set_lineedit()
 }
 void calculator_gui::set_button()
 {
+	QPushButton* btn;
+	int x = 30, y = 400, btn_wid = 80, btn_hei = 30;
+	for (int i = 0; i < 4; i++)
+		for (int j = 0; j < 4; j++)
+		{
+			btn=new QPushButton(this);
+			btn->setText(btn_fun_name[i*4+j]);
+			btn->resize(btn_wid, btn_hei);
+			btn->move(x + btn_wid * j, y + btn_hei * i);
+			connect(btn, SIGNAL(clicked()), this, SLOT(get_input()));
+		}
+	for (int i = 0; i < 4; i++)
+		for (int j = 0; j < 6; j++)
+		{
+			btn= new QPushButton(this);
+			btn->setText(btn_num_name[i * 6 + j]);
+			btn->resize(btn_wid, btn_hei);
+			btn->move(x + btn_wid * j + 350, y + btn_hei * i);
+			connect(btn, SIGNAL(clicked()), this, SLOT(get_input()));
+		}
 	key = new QShortcut(QKeySequence(Qt::Key_Return), this);
 	confirm = new QPushButton(this);
 	confirm->setShortcut(Qt::Key_Enter);
 	connect(key, SIGNAL(activated()), this, SLOT(get_outcome()));//连接到指定槽函数
 	connect(confirm, SIGNAL(clicked()), this, SLOT(get_outcome()));
 	confirm->setText(codec->toUnicode("计算"));
-	confirm->move(650, 400);
+	confirm->move(750, 300);
 	//output->resize(60, 40);
 	font.setPointSize(20);
 	confirm->setFont(this->font);
@@ -79,5 +101,34 @@ void calculator_gui::get_outcome()
 		{
 			output->setText(QString::fromStdString(error));
 		}
+	}
+}
+void calculator_gui::get_input()
+{
+	QPushButton* btn = dynamic_cast<QPushButton*>(sender());
+	if (std::find(btn_fun_name.begin(), btn_fun_name.end(), btn->text()) != btn_fun_name.end())
+	{
+		input_str.push_back(btn->text() + "(");
+	}
+	else if (std::find(btn_num_name.begin(), btn_num_name.end(), btn->text()) != btn_num_name.end())
+	{
+		if (btn->text() == "del" && !input_str.empty())
+			input_str.pop_back();
+		else if (btn->text() == "del" && input_str.empty())
+			return;
+		else if (btn->text() == "=")
+		{
+			get_outcome();
+			return;
+		}
+		else 
+			input_str.push_back(btn->text());
+	}
+	std::vector<QString>::iterator index = input_str.begin();
+	input->clear();
+	while (index != input_str.end())
+	{
+		input->insert(*index);
+		index ++;
 	}
 }
--- cpp_homework_gui_version/calculator_gui.h
+++ cpp_homework_gui_version/calculator_gui.h
@@ -9,16 +9,33 @@
 #include <QShortcut>
 #include <QString>
 #include <QIcon>
+#include <vector>
 class calculator_gui : public QWidget, private calculator_shell
 {
 	Q_OBJECT
 public:
 	calculator_gui(QWidget* parent = 0);
+	//~calculator_gui();
 private:
 	QLineEdit* input;
 	QLineEdit* output;
 	QFont font;
 	QPushButton* confirm;
+	vector<QString> input_str;
+
+	vector<QString> btn_fun_name = {
+		"sqrt", "cos", "sin", "tan",
+		"arcsin", "arccos", "arctan", "sinh",
+		"cosh", "tanh", "arcsinh", "arccosh",
+		"arctanh", "lg", "ln", "abs"
+	};
+	vector<QString> btn_num_name = {
+		"!","1","2","3","+","del",
+		"%","4","5","6","-","(",
+		"pi","7","8","9","*",")",
+		"e","ans","0","^","/","="
+	};
+
 	QShortcut* key;//创建一个快捷键"Key_Return"键
 	QTextCodec* codec = QTextCodec::codecForName("GBK");//中文字体管理 
 
@@ -29,5 +46,6 @@ private:
 
 private slots:
 	void get_outcome();
+	void get_input();
 };
 
